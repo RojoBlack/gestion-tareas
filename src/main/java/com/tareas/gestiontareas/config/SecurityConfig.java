a0ec7a35dad3a4a898a616917a063d5459faeaac
René Alarcón <rojoblack1@gmail.com>
2024-08-17 12:58:02 -0400

    commit 2
    Nueva forma de configurar httpBasic

--- src/main/java/com/tareas/gestiontareas/config/SecurityConfig.java
+++ src/main/java/com/tareas/gestiontareas/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -14,7 +15,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
-                .httpBasic(); // Para autenticación básica
+                .httpBasic(Customizer.withDefaults()); // Nueva forma de configurar httpBasic
 
         return http.build();
     }
