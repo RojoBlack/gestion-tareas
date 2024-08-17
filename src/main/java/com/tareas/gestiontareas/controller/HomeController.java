0dba9093082ef5a060e31de5115e60532ead56ce
René Alarcón <rojoblack1@gmail.com>
2024-08-17 17:15:49 -0400

    commit 4
    Se logra completar la petición http

---
+++ src/main/java/com/tareas/gestiontareas/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.tareas.gestiontareas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Bienvenido al Sistema de Gestión de Tareas";
+    }
+}
